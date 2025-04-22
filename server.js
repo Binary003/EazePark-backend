0c87a8b423d278a043ad519b646f8dbce2a2e8a2
Anuj Verma <138612580+Binary003@users.noreply.github.com>
2025-04-22 21:40:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors({
-  origin: ['https://eazepark.vercel.app', 'http://localhost:3000'],
+  origin: ['https://eazepark.vercel.app', 'http://localhost:3306'],
   credentials: true
 }));
 
