58e479ac840a437b921d87ece92729bdb57804f2
Anuj Verma <138612580+Binary003@users.noreply.github.com>
2025-04-22 21:52:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -165,7 +165,7 @@ app.post("/api/v1/posts", async (req, res) => {
 
 
 // ✅ Start Server
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
